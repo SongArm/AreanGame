12f9a8cbd043b0cda3782538bdbbc43cd6ee7313
SongArm <113346555+SongArm@users.noreply.github.com>
2024-05-28 18:03:19 +0300

    Add project files.

---
+++ ArenaGame/Heroes/Druid.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArenaGame.Heroes
+{
+    public class Druid : Hero
+    {
+        private double healProbability;
+        private double healAmount;
+
+        public Druid(string name, double armor, double strength, IWeapon weapon) : base(name, armor, strength, weapon)
+        {
+            healProbability = 0.15;
+            healAmount = 10;
+        }
+
+        public override double Attack()
+        {
+            double damage = base.Attack();
+            return damage;
+        }
+
+        public override double Defend(double damage)
+        {
+            double probability = random.NextDouble();
+            if (probability < healProbability)
+            {
+                Armor += healAmount;
+                
+            }
+            return base.Defend(damage);
+        }
+    }
+}
+
+
---
+++ ArenaGame/Heroes/Paladin.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArenaGame.Heroes
+{
+    public class Paladin : Hero
+    {
+        private double smiteProbability;
+        private double additionalDamageMin;
+        private double additionalDamageMax;
+
+        public Paladin(string name, double armor, double strength, IWeapon weapon) : base(name, armor, strength, weapon)
+        {
+            smiteProbability = 0.20;
+            additionalDamageMin = 10;
+            additionalDamageMax = 20;
+        }
+
+        public override double Attack()
+        {
+            double damage = base.Attack();
+            double probability = random.NextDouble();
+            if (probability < smiteProbability)
+            {
+                double additionalDamage = random.NextDouble() * (additionalDamageMax - additionalDamageMin) + additionalDamageMin;
+                damage += additionalDamage;
+                
+            }
+            return damage;
+        }
+
+        public override double Defend(double damage)
+        {
+            return base.Defend(damage);
+        }
+    }
+}
+
+
